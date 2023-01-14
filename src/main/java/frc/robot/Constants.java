3fa8d29d704b9205a8414abebb78a6645dfdbf4e
GingerJake <gingerjakey@outlook.com>
2023-01-14 15:41:01 -0500

    Try to wrap our heads around kinematics (it didn't work)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,36 +1,17 @@
 package frc.robot;
 
 public class Constants {
-  // PCM
-  public static final int kPCMId = 0;
-  public static final int kIntakeSolenoidForwardId = 2;
+  //Swerve
+  public static final int k_talFL = 0;
+  public static final int k_talFR = 0;
+  public static final int k_talBL = 0;
+  public static final int k_talBR = 0;
 
-  // DIO
-  public static final int kExtensionUpperLimitPort = 2;
-  public static final int kExtensionLowerLimitPort = 1;
-  public static final int kPivotLowerLimitPort = 0;
+  public static final int k_neoFL = 0;
+  public static final int k_neoFR = 0;
+  public static final int k_neoBL = 0;
+  public static final int k_neoBR = 0;
 
-  // Intake
-  public static final int kIntakeMasterId = 14;
-
-  // Elevator
-  public static final int kElevatorPivotMotorId = 10;
-  public static final int kElevatorExtensionMotorId = 9;
-
-  // Drivetrain
-  public static final int kDrivetrainFLMotorId = 5;
-  public static final int kDrivetrainBLMotorId = 6;
-  public static final int kDrivetrainFRMotorId = 7;
-  public static final int kDrivetrainBRMotorId = 8;
-
-  // Pivot set points
-  public static final double kPivotGroundCount = 0;
-  public static final double kPivotScoreCount = -55;
-  public static final double kPivotPreScoreCount = -65;
-  public static final double kPivotStowCount = -120;
-
-  // Extension set points
-  public static final double kExtensionStowCount = 0;
-  public static final double kExtensionMidGoalCount = 34;
-  public static final double kExtensionHighGoalCount = 72;
+  public static final double k_xDistance = 0.000;
+  public static final double k_yDistance = 0.000;
 }
