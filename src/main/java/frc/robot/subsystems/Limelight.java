c658fc817491cb2aeca10d7fa132727c9202a7c5
kryllyxofficial01 <kryllyxofficial@gmail.com>
2023-01-21 10:37:53 -0500

    add more detailed info

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -11,6 +11,10 @@ public class Limelight {
     private double horizontalOffset;
     private double verticalOffset;
     private double targetArea;
+    private double boundingBoxHorizontal;
+    private double boundingBoxVertical;
+    private int primaryAprilTagID;
+    private Object botPos;
 
     public Limelight() {
         limelight = NetworkTableInstance.getDefault().getTable("limelight");
@@ -21,10 +25,17 @@ public class Limelight {
         horizontalOffset = limelight.getEntry("tx").getDouble(0);
         verticalOffset = limelight.getEntry("ty").getDouble(0);
         targetArea = limelight.getEntry("ta").getDouble(0);
-
+        boundingBoxHorizontal = limelight.getEntry("thor").getDouble(0);
+        boundingBoxVertical = limelight.getEntry("tvert").getDouble(0);
+        primaryAprilTagID = (int) limelight.getEntry("tvert").getInteger(-1); // Uses -1 as default so when no AprilTags are found, we can use this alongside the valid target checker
+        // botPos = limelight.getEntry("botpose").getClass(); // TODO: Find out return type(s) once the limelight is up and running
+        
         SmartDashboard.putBoolean("HasValidTargets", hasValidTargets);
         SmartDashboard.putNumber("HorizontalOffset", horizontalOffset);
         SmartDashboard.putNumber("VerticalOffset", verticalOffset);
         SmartDashboard.putNumber("TargetArea", targetArea);
+        SmartDashboard.putNumber("BoundingBoxVertical", boundingBoxVertical);
+        SmartDashboard.putNumber("BoundingBoxHorizontal", boundingBoxHorizontal);
+        SmartDashboard.putNumber("PrimaryAprilTagID", primaryAprilTagID);
     }
 }
