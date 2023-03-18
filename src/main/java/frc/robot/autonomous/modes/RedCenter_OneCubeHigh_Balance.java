e2c96cca07322458e61ba54eecfd9ca38d3ef0d0
GingerJake <gingerjakey@outlook.com>
2023-03-17 20:38:00 -0400

    fixed my formatting

--- src/main/java/frc/robot/autonomous/modes/RedCenter_OneCubeHigh_Balance.java
+++ src/main/java/frc/robot/autonomous/modes/RedCenter_OneCubeHigh_Balance.java
@@ -13,54 +13,54 @@ import frc.robot.autonomous.tasks.PointForwardTask;
 import frc.robot.autonomous.tasks.WaitTask;
 
 public class RedCenter_OneCubeHigh_Balance extends AutoModeBase {
-    @Override
-    public Pose2d getStartingPosition() {
-        return new Pose2d(14.7, 2.73, Rotation2d.fromDegrees(180.0));
-    }
+  @Override
+  public Pose2d getStartingPosition() {
+    return new Pose2d(14.7, 2.73, Rotation2d.fromDegrees(180.0));
+  }
 
-    public void queueTasks() {
-        queueTask(new ParallelTask(
-                new PointForwardTask(),
-                new WaitTask(0.5)));
+  public void queueTasks() {
+    queueTask(new ParallelTask(
+        new PointForwardTask(),
+        new WaitTask(0.5)));
 
-        queueTask(new ArmTrajectoryTask(Constants.Arm.Preset.SCORE_HIGH_CUBE.getPose()));
-        queueTask(new GripperTask(false));
+    queueTask(new ArmTrajectoryTask(Constants.Arm.Preset.SCORE_HIGH_CUBE.getPose()));
+    queueTask(new GripperTask(false));
 
-        // queueTask(new ParallelTask( // TODO: DriveForward not working in ParallelTask
-        // new DriveTrajectoryTask("RedLeftFarBalance", 1.0, 0.5),
-        // new ArmTrajectoryTask(Constants.Arm.Preset.HOME.getPose())));
+    // queueTask(new ParallelTask( // TODO: DriveForward not working in ParallelTask
+    // new DriveTrajectoryTask("RedLeftFarBalance", 1.0, 0.5),
+    // new ArmTrajectoryTask(Constants.Arm.Preset.HOME.getPose())));
 
-        queueTask(new ArmTrajectoryTask(Constants.Arm.Preset.HOME.getPose()));
+    queueTask(new ArmTrajectoryTask(Constants.Arm.Preset.HOME.getPose()));
 
-        queueTask(new DriveForwardTask(1.5, 1)); // TODO: Always goes forward in sim
+    queueTask(new DriveForwardTask(1.5, 1)); // TODO: Always goes forward in sim
 
-        queueTask(new AutoBalanceTask());
+    queueTask(new AutoBalanceTask());
 
-        // queueTask(new DriveForwardTask(0, 0));
+    // queueTask(new DriveForwardTask(0, 0));
 
-        // queueTask(new
-        // ArmTrajectoryTask(Constants.Arm.Preset.SCORE_HIGH_CUBE.getPose()));
+    // queueTask(new
+    // ArmTrajectoryTask(Constants.Arm.Preset.SCORE_HIGH_CUBE.getPose()));
 
-        // queueTask(new WaitTask(1.0));
+    // queueTask(new WaitTask(1.0));
 
-        // queueTask(new GripperTask(false));
+    // queueTask(new GripperTask(false));
 
-        // queueTask(new WaitTask(1.0));
+    // queueTask(new WaitTask(1.0));
 
-        // queueTask(
-        // new ParallelTask(
-        // new ArmTrajectoryTask(Constants.Arm.Preset.HOME.getPose()),
-        // new DriveForwardTask(2.3, 1))); // 6 meters is past
+    // queueTask(
+    // new ParallelTask(
+    // new ArmTrajectoryTask(Constants.Arm.Preset.HOME.getPose()),
+    // new DriveForwardTask(2.3, 1))); // 6 meters is past
 
-        // queueTask(new DriveForwardTask(0.5, 0.6));
+    // queueTask(new DriveForwardTask(0.5, 0.6));
 
-        // queueTask(new AutoBalanceTask());
+    // queueTask(new AutoBalanceTask());
 
-        // queueTask(new BrakeTask(true));
+    // queueTask(new BrakeTask(true));
 
-        // queueTask(new WaitTask(0.5));
+    // queueTask(new WaitTask(0.5));
 
-        // queueTask(new DriveForwardTask(-1.0, 0.05));
-    }
+    // queueTask(new DriveForwardTask(-1.0, 0.05));
+  }
 
 }
