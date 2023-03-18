95ff1e01ab2be425e48ffd0f161f18c550ec950c
dracco1993 <dracco1993@gmail.com>
2023-03-18 15:12:10 -0400

    Add back the rest of Right_OneCubeHigh_Balance

--- src/main/java/frc/robot/autonomous/modes/Right_OneCubeHigh_Balance.java
+++ src/main/java/frc/robot/autonomous/modes/Right_OneCubeHigh_Balance.java
@@ -4,10 +4,13 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import frc.robot.Constants;
 import frc.robot.autonomous.tasks.ArmTrajectoryTask;
+import frc.robot.autonomous.tasks.AutoBalanceTask;
 import frc.robot.autonomous.tasks.DriveForwardTask;
 import frc.robot.autonomous.tasks.DriveTrajectoryTask;
+import frc.robot.autonomous.tasks.GripperTask;
 import frc.robot.autonomous.tasks.ParallelTask;
 import frc.robot.autonomous.tasks.PointForwardTask;
+import frc.robot.autonomous.tasks.WaitTask;
 
 public class Right_OneCubeHigh_Balance extends AutoModeBase {
   @Override
@@ -18,14 +21,13 @@ public class Right_OneCubeHigh_Balance extends AutoModeBase {
   public void queueTasks() {
     queueTask(new PointForwardTask());
 
-    // queueTask(new
-    // ArmTrajectoryTask(Constants.Arm.Preset.SCORE_HIGH_CUBE.getPose()));
+    queueTask(new ArmTrajectoryTask(Constants.Arm.Preset.SCORE_HIGH_CUBE.getPose()));
 
-    // queueTask(new WaitTask(1.0));
+    queueTask(new WaitTask(1.0));
 
-    // queueTask(new GripperTask(false));
+    queueTask(new GripperTask(false));
 
-    // queueTask(new WaitTask(1.0));
+    queueTask(new WaitTask(1.0));
 
     queueTask(new ParallelTask(
         new ArmTrajectoryTask(Constants.Arm.Preset.HOME.getPose()),
@@ -33,7 +35,7 @@ public class Right_OneCubeHigh_Balance extends AutoModeBase {
 
     queueTask(new DriveForwardTask(2.0, -1.0));
 
-    // queueTask(new AutoBalanceTask());
+    queueTask(new AutoBalanceTask());
   }
 
 }
