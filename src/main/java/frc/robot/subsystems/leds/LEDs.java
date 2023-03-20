9beec37f8e62d88b771e26ac833a81258ea5fbf3
GingerJakeDaBoi <gingerjakey@outlook.com>
2023-03-20 17:18:11 -0400

    fix unimplemented method in leds

--- src/main/java/frc/robot/subsystems/leds/LEDs.java
+++ src/main/java/frc/robot/subsystems/leds/LEDs.java
@@ -67,4 +67,10 @@ public class LEDs extends Subsystem {
   @Override
   public void outputTelemetry() {
   }
+
+  @Override
+  public void writeToLog() {
+    // TODO Auto-generated method stub
+    
+  }
 }
