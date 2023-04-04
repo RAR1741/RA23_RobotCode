5e68134dd02a82cf3b9a3dab864c5a522b506149
dracco1993 <dracco1993@gmail.com>
2023-04-04 19:29:57 -0400

    More LL stuff

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -1,24 +1,29 @@
 package frc.robot.subsystems;
 
-import java.util.Arrays;
 import java.util.HashMap;
 
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.LimelightHelpers;
 
 public class Limelight extends Subsystem {
   private static Limelight m_limelight;
   private NetworkTable m_limelightTable;
   private HashMap<String, Object> m_limelightInfo;
+  private String m_name = "limelight";
+
+  // Limelight Offsets:
+  // X: 12 5426 in
+  // Y: 9.19 in
+  // Z: 10.1175 in
 
   /**
    * Constructor
    */
   private Limelight() {
-    m_limelightTable = NetworkTableInstance.getDefault().getTable("limelight");
+    m_limelightTable = NetworkTableInstance.getDefault().getTable(m_name);
     m_limelightInfo = new HashMap<String, Object>();
   }
 
@@ -47,7 +52,7 @@ public class Limelight extends Subsystem {
    * @return Current bot pose
    */
   public Pose2d getBotpose2D() {
-    return LimelightHelpers.getBotPose2d("Limelight");
+    return toFieldPose(LimelightHelpers.getBotPose2d(m_name));
   }
 
   /**
@@ -56,28 +61,11 @@ public class Limelight extends Subsystem {
    * @return If there is a visible AprilTag
    */
   public boolean seesAprilTag() {
-    return (int) m_limelightTable.getEntry("tv").getInteger(0) == 1;
+    return m_limelightTable.getEntry("tv").getInteger(0) == 1;
   }
 
   @Override
   public void periodic() {
-    for (String key : this.m_limelightTable.getKeys()) {
-      String type = this.m_limelightTable.getEntry(key).getType().name().substring(1);
-
-      if (type.equals("String")) {
-        this.m_limelightInfo.put(key, this.m_limelightTable.getEntry(key).getString("No value"));
-      } else if (type.equals("Double")) {
-        this.m_limelightInfo.put(key, this.m_limelightTable.getEntry(key).getDouble(Double.NaN));
-      } else if (type.equals("DoubleArray")) {
-        this.m_limelightInfo.put(key, this.m_limelightTable.getEntry(key).getDoubleArray(new double[6]));
-      }
-
-      if (type.equals("String") || type.equals("Double")) {
-        SmartDashboard.putString(key, this.m_limelightInfo.get(key).toString());
-      } else {
-        SmartDashboard.putString(key, Arrays.toString((double[]) m_limelightInfo.get(key)));
-      }
-    }
   }
 
   @Override
@@ -90,5 +78,15 @@ public class Limelight extends Subsystem {
 
   @Override
   public void outputTelemetry() {
+  }
+
+  /**
+   * Converts the limelight coordinate system to the field coordinate system.
+   *
+   * @param pose Position of the robot
+   * @return The position of the robot in terms of the field.
+   */
+  private Pose2d toFieldPose(Pose2d pose) {
+    return pose.relativeTo(new Pose2d(-8.2296, -8.2296 / 2, Rotation2d.fromDegrees(0)));
   }
 }
